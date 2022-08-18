54b028ccd154902d3a3358670f42e834a5ed2e79
ywenhao <ywenhao2019@qq.com>
2022-08-18 17:36:01 +0800

    feat: 修改注释

--- types/shims-vue.d.ts
+++ types/shims-vue.d.ts
@@ -3,6 +3,9 @@ import { EventKeys } from "./event-keys";
 
 declare module "vue/types/vue" {
   interface Vue {
+    /**
+     * 全局互相调用event的dispatch
+     */
     globalDispatch: (eventKey: EventKeys, ...args: any[]) => any;
   }
 }
